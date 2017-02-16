cdac8573466cd73c0896583b7baa9b47848deb3d
RGeetha24 <rgeetha2296@gmail.com>
2017-02-16 20:13:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,8 @@
 console.log('Loaded!');
 //change the text of main text div
 var element=document.getElementById('maint');
-element.innerHTML='geetha';
+element.innerHTML='haiiiii...this is geetha';
+
 var im=document.getElementById('im');
 im.onclick=function(){
     im.style.marginleft='100px';
