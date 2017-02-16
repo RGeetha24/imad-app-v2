4eca0d7e99c72419dd400101a0f48c5d2e1f7fb6
RGeetha24 <rgeetha2296@gmail.com>
2017-02-16 21:21:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,9 @@
 console.log('Loaded!');
 //change the text of main text div
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,100);
+var button=document.getElementbyId('counter');
+var count=0;
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
