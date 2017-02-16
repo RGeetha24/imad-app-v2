6f35730071beafed9092e600e6c77a034d6e3052
RGeetha24 <rgeetha2296@gmail.com>
2017-02-16 20:10:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 //change the text of main text div
 var element=document.getElementById('maint');
 element.innerHTML='geetha';
+var im=document.getElementById('im');
+im.onclick=function(){
+    im.style.marginleft='100px';
+};
