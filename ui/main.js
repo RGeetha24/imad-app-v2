249604f72512061552c4b789e88ee05a32393f09
RGeetha24 <rgeetha2296@gmail.com>
2017-02-16 21:24:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 //change the text of main text div
 var button=document.getElementbyId('counter');
-var count=0;
+var counter=0;
 button.onclick=function(){
     counter=counter+1;
     var span=document.getElementById('count');
