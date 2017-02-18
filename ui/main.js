bcdca1ee3e6c27681b563c84845dbc0817adc98b
RGeetha24 <rgeetha2296@gmail.com>
2017-02-18 21:14:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,12 +7,12 @@ button.onclick=function(){
     span.innerHTML=counter.toString();
 };
 var nameInput=document.getElementById('name');
-var name=nameInput.value;
+var nam=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     var names={'name1','name2','name3','name4'};
     var list='';
-    for(var i=0;i=names.length;i++){
+    for(var i=0;i==names.length;i++){
         list=='<li>'+names[i]+'</li>';
     }
     var ul=document.getElementById('namelist');
